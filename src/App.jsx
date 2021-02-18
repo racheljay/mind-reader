53ae5a1f296a30a6ff913b05434cbd5d17bc8157
Rachel <racheljay42@gmail.com>
2021-02-17 20:16:05 -0500

    starting fresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import Content from './Content';
+// import Content from './Content';
 
-// import logo from './logo.svg';
-// import './App.css';
 
 function App() {
 
-  //page Content Object
-  
+  const [pageNum, setPageNum] = useState(0)
 
-//declare initial state
-const [currentState, setState] = useState(0);
-
-//component did mount
-useEffect(() => {
-  console.log('did mount')
-  let lsState = window.localStorage.getItem('currentState')
-  if(lsState) {
-    if(lsState !== currentState) {
-      setState(JSON.parse(lsState))
+  const pages = ["<Page0/>", "<Page1/>"]
+  const clickNext = () => {
+    if(pageNum < 6) {
+      setPageNum(pageNum + 1)
     }
   }
-  console.log(currentState)
-}, [])
 
-//component did update
-const setNewState = (newState) => {
-  setState(newState)
-  window.localStorage.setItem('currentState', JSON.stringify(newState))
-}
-
-//we may not actually need this. stand by
-function init() {
-  setNewState(0);
-  // heading.textContent = pageContent[state].heading;
-  // pageNumber.innerHTML = pageContent[state].pgnum;
-  // subHead.innerHTML = pageContent[state].desc;
-}
-
-function next() {
-  if(currentState <= 4) {
-    setNewState(currentState + 1)
+  const clickBack = () => {
+    if(pageNum > 0) {
+      setPageNum(pageNum - 1)
+    }
   }
-  console.log("currenc state ", currentState)
-}
-
-function back() {
-  if (currentState >= 1) {
-    setNewState(currentState - 1)
+  const clickHome = () => {
+    setPageNum(0)
   }
-  console.log("current state ", currentState)
-}
 
+  const content = () => {
+    return (
+      JSON.parse(pages[pageNum])
+    )
+  }
 
   return (
-    <>
-      <h1>Mind Reader</h1>
+    <div className="container">
+      <h1>Current Page {pageNum}</h1>
 
+      {content}
 
+      <button className="btn btn-danger" onClick={clickHome}>Home</button>
+      <button className="btn btn-danger" onClick={clickBack}>Back</button>
+      <button className="btn btn-danger" onClick={clickNext}>Next</button>
 
-    <Content
-      currentState={currentState}
-     />
-
-      <div className="container">
-        <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-light justify-content-center">
-          <ul className="nav">
-            <li className="nav-item">
-              <button
-              id="home" 
-              className="btn btn-warning"
-              >
-                Home
-                </button>
-            </li>
-            <li className="nav-item">
-              <button
-              onClick={back}
-              className="btn btn-warning"
-              >
-                Back
-                </button>
-            </li>
-            <li className="nav-item">
-              <button
-              onClick={next}
-              className="btn btn-warning"
-              >
-                Next</button>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
-  );
+    </div>
+  )
 }
 
 export default App;
+
+const Page0 = () => {
+  return (
+    <h1>I can read your mind</h1>
+  )
+}
+
+const Page1 = () => {
+  return (
+    <h1>page2</h1>
+  )
+}
+
+const Page2 = () => {
+  return (
+    <h1>page3</h1>
+  )
+}
+
+const Page3 = () => {
+  return (
+    <h1>page4</h1>
+  )
+}
+
+const Page4 = () => {
+  return (
+    <h1>page5</h1>
+  )
+}
+
+const Page5 = () => {
+  return (
+    <h1>page6</h1>
+  )
+}
