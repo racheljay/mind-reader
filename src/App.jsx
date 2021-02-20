190bae367523f2f1421734764ecf0916faadde61
Rachel <racheljay42@gmail.com>
2021-02-19 21:30:13 -0500

    changed list from string to object and render on page

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   const [pageNum, setPageNum] = useState(0)
+  const [listData, setListData] = useState([])
+  const [solution, setSolution] = useState("")
 
-  const pages = ["<Page0/>", "<Page1/>"]
   const clickNext = () => {
     if (pageNum < 5) {
       setPageNum(pageNum + 1)
@@ -48,18 +49,59 @@ function App() {
       desc: ["Is that your letter?"],
     }];
 
+    //pair random letters with numbers
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+let list = [];
+
+function rando() {
+    return Math.floor(Math.random() * (9 - 0 + 1) + 0);
+}
+
+var trick = (letters[rando()]);
+
+function stringlist() {
+
+    for (let i = 0; i <= 99; i++) {
+        if (i % 9 === 0) {
+            // list += i + " - " + trick;
+            list.push({num: i, letter: trick})
+        } else if (i % 9 !== 0) {
+            // list += i + " - " + letters[rando()];
+            list.push({num: i, letter: letters[rando()]})
+        } 
+    }
+    return list;
+}
+
+useEffect(() => {
+  console.log("use effect")
+  setListData(stringlist())
+  // console.log(listData[0].letter)
+
+}, [])
+
   return (
     <div className="container">
       <h1>Current Page {pageNum}</h1>
+
+      <div className="container">
 
       {pageNum === 0 && <h1 className="display1">I can read your mind...</h1>}
 
       {pageNum > 0 && pageNum < 4 && <h1>{pageContent[(pageNum - 1)].heading}</h1>}
 
+      </div>
+    
+
       <button className="btn btn-danger" onClick={clickHome}>Home</button>
       <button className="btn btn-danger" onClick={clickBack}>Back</button>
       <button className="btn btn-danger" onClick={clickNext}>Next</button>
-      <p>Page {(pageNum + 1)} of 6</p>
+      <p>Page {(pageNum + 1)} of 6 </p>
+    {listData.map((item, index) => {
+      return(
+        <div key={index}>{item.num} - {item.letter}</div>
+      )
+    })}
 
     </div>
   )
